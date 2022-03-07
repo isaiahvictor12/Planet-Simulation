70a286f0e1e826f5e3b63b2691710efcbaf7f27a
Isaiah Victor <ikvictor@iu.edu>
2022-03-07 04:29:43 -0500

    All 8 planets + sun are added

--- Planet Simulation/main.py
+++ Planet Simulation/main.py
@@ -24,19 +24,25 @@ def main():
     mercury = Planet(0.387 * Planet.AU, 0, 14, settings.DARK_GREY, 3.3 * 10**23, "Mercury")
     mercury.y_vel = -47.4 * 1000
 
-    venus = Planet(0.723 * Planet.AU, 0, 14, settings.WHITE, 4.8685 * 10**24, "Venus")
+    venus = Planet(0.723 * Planet.AU, 0, 14, settings.YELLOW, 4.8685 * 10**24, "Venus")
     venus.y_vel = -35.02 * 1000
 
     jupiter = Planet(5.204 * Planet.AU, 0, 22, settings.ORANGE, 1898 * 10**24, "Jupiter")
     jupiter.y_vel = -13.1 * 1000
 
-    saturn = Planet(9.572 * Planet.AU, 0, 20, settings.ORANGE, 568 * 10**24, "Saturn")
+    saturn = Planet(9.572 * Planet.AU, 0, 20, settings.PURPLE, 568 * 10**24, "Saturn")
     saturn.y_vel = -9.7 * 1000
 
-    planets = [sun, earth, mars, mercury, venus, jupiter, saturn]
+    uranus = Planet(19.164 * Planet.AU, 0, 17, settings.WHITE, 86.8 * 10**24, "Uranus")
+    uranus.y_vel = -6.8 * 1000
+
+    neptune = Planet(30.18 * Planet.AU, 0, 17, settings.BLUE_NEPTUNE, 102 * 10**24, "Neptune")
+    neptune.y_vel = -5.4 * 1000
+
+    planets = [sun, earth, mars, mercury, venus, jupiter, saturn, uranus, neptune]
 
     while run:
-        clock.tick(60)
+        #clock.tick(60)
         WIN.fill((0, 0, 0))
         # WIN.fill(WHITE)
         # pygame.display.update()
